563759c87856aaf012f4f76c0fc2dd7134663834
andrewperrone <152441315+andrewperrone@users.noreply.github.com>
2023-12-06 01:47:04 -0500

    Update PremierLeague_dbCreation.sql
    Changed size of tabs for consistency with workbench
    Cleaned up tabs
    Set default season_id for fixtures
    Added query to change default
    Began adding constraint to fantasyTeamMembers (fantasyTeamMember_onlyUniquePlayers)

--- PremierLeague_dbCreation.sql
+++ PremierLeague_dbCreation.sql
@@ -4,21 +4,33 @@ use pl;
 
 create table if not exists season
 (
-	season_id int PRIMARY KEY auto_increment,
-    startDate date not null,
-    endDate date not null
-);
-
-create table if not exists fantasyLeague
-(
-	fantasyleague_id 		int 		PRIMARY KEY auto_increment, -- Primary Key
-    fantasyleague_name 		varchar(50) not null
+	season_id 	int 	PRIMARY KEY auto_increment,
+    start_date 	date 	not null,
+    end_date 	date 	not null
 );
 
 create table if not exists league
 (
-	league_id 		int 		PRIMARY KEY auto_increment, -- Primary Key
-	league_name 	varchar(50) not null
+	league_id 	int 		PRIMARY KEY auto_increment, -- Primary Key
+	league_name 	varchar(50) 	not null
+);
+
+create table if not exists fantasyLeague
+(
+	fantasyleague_id 	int 		PRIMARY KEY auto_increment, -- Primary Key
+    fantasyleague_name 	varchar(50) 	not null
+);
+
+create table if not exists team
+(
+	team_id 	int 		PRIMARY KEY auto_increment, -- Primary Key
+   	team_name	varchar(50) not null,
+    league_id 	int 		default(1) not null,
+    
+    constraint team_fk_league
+		foreign key (league_id)
+			references league(league_id)
+				on delete no action
 );
 
 create table if not exists userTeam
@@ -29,29 +41,16 @@ create table if not exists userTeam
     team_name		varchar(50) not null,
     email 			varchar(50) default(null), -- Normally would not default to null but we probably won't fill these columns out
     `password` 		varchar(50) default(null),  -- Going to use them to practice views (maybe security too?)
-    budget 			int			default(100) not null
+    budget 			int	default(100) not null
     -- TODO make sure budget >= 0 AND budget <= 100
 );
-
 -- TODO max 3 players from a real team in a fantasy team
-
-create table if not exists team
-(
-	team_id 		int 		PRIMARY KEY auto_increment, -- Primary Key
-    team_name		varchar(50) not null,
-    league_id 		int 		default(1) not null,
-    
-    constraint team_fk_league
-		foreign key (league_id)
-			references league(league_id)
-				on delete no action
-);
 
 create table if not exists fantasyLeagueMember
 (
-	userteam_id int not null,
-    fantasyleague_id int not null,
-    creator bool default(0),
+	userteam_id 		int not null,
+    fantasyleague_id 	int not null,
+    creator 			bool default(0),
     
     constraint fantasyLeagueMember_pk
 		primary key (userteam_id,fantasyleague_id),
@@ -67,13 +66,13 @@ create table if not exists fantasyLeagueMember
 
 create table if not exists player
 (
-	player_id 		int 		PRIMARY KEY auto_increment,
-    	player_name 		varchar(50) not null,
-   	cost			int 		default(5) not null,
-    	pos ENUM('GK', 'D', 'M', 'F')  not null,
-	team_id			int,
-    	league_id		int 		default(1) not null,
-    	constraint player_fk_team
+	player_id 	int 		PRIMARY KEY auto_increment,
+    player_name varchar(50) not null,
+   	cost		int 		default(5) not null,
+    pos ENUM('GK', 'D', 'M', 'F')	not null,
+	team_id		int,
+    league_id	int 		default(1) not null,
+    constraint player_fk_team
 		foreign key (team_id)
 			references team(team_id)
 				on delete no action,
@@ -86,10 +85,10 @@ create table if not exists player
 create table if not exists fantasyTeamMember
 (
 	pos ENUM('GK', 'LB', 'CLB', 'CRB', 'RB', 'DM', 'LM', 'CLM', 'CRM', 'RM', 'ST', 'R1', 'R2', 'R3', 'R4') not null,
-    userteam_id 	int			not null,
-    player_id 		int 		not null,
+    userteam_id int		not null,
+    player_id 	int 	not null,
     
-    constraint fantasyTeamMember_pk
+   	 constraint fantasyTeamMember_pk
 		primary key (pos, userteam_id),
 	constraint fantasyTeamMember_fk_userteam
 		foreign key (userteam_id)
@@ -99,24 +98,26 @@ create table if not exists fantasyTeamMember
 		foreign key (player_id)
 			references player(player_id)
 				on delete no action
+	-- constraint fantasyTeamMember_onlyUniquePlayers
+		-- check 
 );
 
 create table if not exists fixture
 (
-	fixture_id 		int		PRIMARY KEY auto_increment,
-	gameweek		int 	not null,
-    	fixture_date 	date 	not null,
-    	location		date	not null,
-    	league_id		int 	default(1) not null,
-    	season_id		int		not null,
-    	home_team_id	int 	not null,
-    	home_team_goals	int		not null,
-    	away_team_id	int 	not null,
-    	away_team_goals	int		not null,
-    	forfeit			bool	default(0),
-    	draw 			bool 	default(0),
+	fixture_id 		int			PRIMARY KEY auto_increment,
+	gameweek		int 		not null,
+    fixture_date 	date 		not null,
+    location		varchar(50)	not null,
+    league_id		int 		default(1) not null,
+    season_id		int			default(1) not null,
+    home_team_id	int 		not null,
+    home_team_goals	int			not null,
+    away_team_id	int 		not null,
+    away_team_goals	int			not null,
+    forfeit			bool		default(0),
+    draw 			bool 		default(0),
     
-    	constraint fixture_fk_league
+    constraint fixture_fk_league
 		foreign key (league_id)
 			references league(league_id)
 				on delete no action,
@@ -133,6 +134,11 @@ create table if not exists fixture
 			references team(team_id)
 				on delete no action
 );
+
+-- Sets the default to the current season
+alter table fixture
+alter column season_id set default(22);
+-- TODO automatically find current season
 
 create table if not exists fantasyFixture
 (
@@ -165,6 +171,11 @@ create table if not exists fantasyFixture
 				on delete no action
 );
 
+-- Sets the default to the current season
+alter table fantasyFixture
+alter column season_id set default(22);
+-- TODO automatically find current season
+
 create table if not exists teamStats
 (
 	team_id				int 	not null,
@@ -188,7 +199,6 @@ create table if not exists teamStats
 				on delete no action
 	
 );
-
 
 create table if not exists userTeamStats
 (
@@ -240,7 +250,6 @@ create table if not exists playerStats
 			references season(season_id)
 				on delete no action
 );
-
 
 -- Manual Data Input
 insert into league values

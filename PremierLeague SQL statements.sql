93101b6d66b4f638d33a9c9a4e532e853018cb3b
andrewperrone <152441315+andrewperrone@users.noreply.github.com>
2023-12-06 14:28:47 -0500

    Update PremierLeague SQL statements.sql
    changed everything

--- PremierLeague SQL statements.sql
+++ PremierLeague SQL statements.sql
@@ -1,62 +1,54 @@
 
 #1.Retrieve all players from the team Arsenal:
-SELECT player.player_id, player.player_name
+SELECT player_id, player_name
 FROM player
-JOIN team ON player.team_id = team.team_id
-WHERE team.team_name = 'Arsenal';
+WHERE team_id = (select team_id from team where team_name like '%Arsenal%');
 
-#2. Get the total points scored by a users fantasy team in a specific season
-SELECT userteam.team_name, SUM(playerstats.points) AS total_points
-FROM userteam
-JOIN playerstats ON userteam.userteam_id = playerstats.userteam_id
-WHERE userteam.user_id = 1001 AND playerstats.season_id = 1
-GROUP BY userteam.team_name;
+#2. Retrieve shots blocked, clearences, goals conceded for all Arsenal Players
+select p.player_id, p.player_name, s.blocked_shots, s.clearance, s.goals_conceded
+from player p, playerStats s
+where p.player_id = s.player_id and p.team_id = (select team_id from team where team_name like '%Arsenal%');
 
 #3. Retrieve all users in a fantasy league
-SELECT users.user_id, users.username, fantasyleague.league_name
-FROM users
-JOIN league ON users.user_id = league.user_id
-JOIN fantasyleague ON league.league_id = fantasyleague.league_id
-WHERE fantasyleague.season_id = 1;
+select m.fantasyleague_id, 
+	(select fantasyleague_name from fantasyLeague l where l.fantasyleague_id = m.fantasyleague_id) as fantasyleague_name, 
+    m.userteam_id, 
+    (select display_name from userTeam u where u.userteam_id = m.userteam_id) as display_name, 
+    m.creator
+from fantasyLeagueMember m
+where m.fantasyleague_id = 1;
 
-#4Retrieve the upcoming fixtures for a specific team:
-SELECT fixture.fixture_id, home_team.team_name AS home_team, away_team.team_name AS away_team, fixture.date
+#4 Retrieve all fixtures a Arsenal has played
+SELECT *, (select team_name from team where team_name like '%Arsenal%') as team_name
 FROM fixture
-JOIN team AS home_team ON fixture.home_team_id = home_team.team_id
-JOIN team AS away_team ON fixture.away_team_id = away_team.team_id
-WHERE (home_team.team_name = 'Chelsea' OR away_team.team_name = 'Chelsea') AND fixture.date > CURDATE()
-ORDER BY fixture.date;
+WHERE home_team_id = (select team_id from team where team_name like '%Arsenal%') or away_team_id = (select team_id from team where team_name like '%Arsenal%') 
+ORDER BY fixture_date;
 
--- Sample SQL DML Statements
-
-
-/*
-SELECT * from fantasyFixtures
-WHERE SeasonID = 22;
-
-SELECT * from UserTeamStats
-WHERE SeasonID = 22;
-
-SELECT * from UserTeamStats
-Where fantasyLeagueID = 256;
-
-SELECT * from fantasyFixtures
-WHERE fantasyLeagueID = 256;
-
-SELECT creator from fantasyLeagueMember
-WHERE UserTeamId = 10;
-
-SELECT * from fantasyTeamMember
-WHERE UserTeamID = 10;
-
-SELECT stats from PlayerStats
-Where PlayerID = 25 and SeasonID = 22;
-
-SELECT * from Player
-WHERE TeamID = 14;
-
-SELECT * from Fixtures
-WHERE TeamID = 14;
-
-
-
+#5 All the steps for running a fantasyFixture
+-- Query for creating and running a fixture 
+insert into fantasyfixture(fantasyleague_id, season_id, home_team_id, home_team_goals, away_team_id, away_team_goals, home_win, draw) values
+(1, 22, 1, 5*rand() - 1, 2, 5*rand() - 1, home_team_goals > away_team_goals, home_team_goals = away_team_goals);
+-- Update userTeamStats with most recent fixture
+-- Updates Home Team
+update userTeamStats 
+set goals = goals + (select home_team_goals from fantasyfixture where fantasyfixture_id = (select max(fantasyfixture_id) from fantasyfixture)),
+	goals_conceded = goals_conceded + (select away_team_goals from fantasyfixture where fantasyfixture_id = (select max(fantasyfixture_id) from fantasyfixture)),
+    wins = wins + (select home_win from fantasyfixture where fantasyfixture_id = (select max(fantasyfixture_id) from fantasyfixture)),
+    losses = losses + 1 - (select home_win from fantasyfixture where fantasyfixture_id = (select max(fantasyfixture_id) from fantasyfixture)),
+    matches_played = matches_played + 1,
+    clean_sheets = clean_sheets + if((select home_team_goals from fantasyfixture where fantasyfixture_id = (select max(fantasyfixture_id) from fantasyfixture)) > 0 and
+									(select away_team_goals from fantasyfixture where fantasyfixture_id = (select max(fantasyfixture_id) from fantasyfixture)) = 0, 1, 0)
+where userteam_id = (select home_team_id from fantasyfixture where fantasyfixture_id = (select max(fantasyfixture_id) from fantasyfixture));
+-- Updates Away Team
+update userTeamStats 
+set goals = goals + (select away_team_goals from fantasyfixture where fantasyfixture_id = (select max(fantasyfixture_id) from fantasyfixture)),
+	goals_conceded = goals_conceded + (select home_team_goals from fantasyfixture where fantasyfixture_id = (select max(fantasyfixture_id) from fantasyfixture)),
+    wins = wins + 1 - (select home_win from fantasyfixture where fantasyfixture_id = (select max(fantasyfixture_id) from fantasyfixture)),
+    losses = losses + (select home_win from fantasyfixture where fantasyfixture_id = (select max(fantasyfixture_id) from fantasyfixture)),
+    matches_played = matches_played + 1,
+    clean_sheets = clean_sheets + if((select away_team_goals from fantasyfixture where fantasyfixture_id = (select max(fantasyfixture_id) from fantasyfixture)) > 0 and
+									(select home_team_goals from fantasyfixture where fantasyfixture_id = (select max(fantasyfixture_id) from fantasyfixture)) = 0, 1, 0)
+where userteam_id = (select away_team_id from fantasyfixture where fantasyfixture_id = (select max(fantasyfixture_id) from fantasyfixture));
+SELECT * FROM pl.userteamstats;
+-- TODO Update immediately after fixture
+-- TODO Re-make with variables
